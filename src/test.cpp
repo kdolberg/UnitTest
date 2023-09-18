9779269cee021cec117fe6fcc338fdee7275fbcb
krisdolberg <kristofor.d@gmail.com>
2023-09-18 11:04:15 -0700

    Added letter-grade function

--- src/test.cpp
+++ src/test.cpp
@@ -1,4 +1,7 @@
 #include <assert.h>
+#include <vector>
+#include <string>
+#include <sstream>
 #include "test.h"
 
 /**
@@ -81,9 +84,63 @@ void record_result(ResultType t) {
 	}
 }
 
+std::string repeat_character(const char * c, int n) {
+	std::string ret = "";
+	for (int i = 0; i < n; ++i) {
+		ret += *c;
+	}
+	return ret;
+}
+
+#define REPORT_CARD_HEADER_LENGTH 150
+#define REPORT_CARD_HEADING "UNIT TESTING REPORT CARD"
+#define HEADING_CHAR "-"
+#define PASS_THRESHOLD 0.5f
+
+std::string report_card_header(const std::string& heading,int header_length) {
+	std::stringstream ss;
+	ss << std::endl << std::endl;
+	ss << repeat_character(HEADING_CHAR,header_length) << std::endl;
+	ss << repeat_character(" ",(header_length-heading.length())/2) << heading << std::endl;
+	ss << repeat_character(HEADING_CHAR,header_length) << std::endl;
+	ss << std::endl;
+	return ss.str();
+}
+
+std::string letter_grade(float score) {
+	if(score < PASS_THRESHOLD) {
+		return "F";
+	}
+	if(score >= 1.0f) {
+		return "A+";
+	}
+	std::vector<std::string> passing_grades = {"D-","D","D+","-C","C","C+","B-","B","B+","A-","A","A+"};
+	float increment = PASS_THRESHOLD/(passing_grades.size());
+	int index = (int)(((score-PASS_THRESHOLD)/increment));
+	return (passing_grades[index]);
+}
+
+std::string letter_grade() {
+	return letter_grade(percent_pass()*0.01f);
+}
+
+void test_letter_grade() {
+	for (float s = 0.0f; s <= 1.0f; s+=0.005f) {
+		LOG << 100*s << "%" << ":\t" << letter_grade(s) << std::endl;
+	}
+}
+
 void print_report_card() {
-	LOG << percent_pass() << "%" << " (" << total_tests_passed() << " tests) passed\n";
-	LOG << percent_fail() << "%" << " (" << total_tests_failed() << " tests) failed\n";
-	LOG << percent_error_fail() << "%"  << " (" << total_tests_failed_due_to_error() << " tests) failed due to error\n";
+	LOG << report_card_header(REPORT_CARD_HEADING,REPORT_CARD_HEADER_LENGTH);
+
+	// Print relevant statistics for each test
+	if(total_tests_passed())
+		LOG << percent_pass() << "%" << " (" << total_tests_passed() << " tests) passed\n";
+	if(total_tests_failed())
+		LOG << percent_fail() << "%" << " (" << total_tests_failed() << " tests) failed\n";
+	if(total_tests_failed_due_to_error())
+		LOG << percent_error_fail() << "%"  << " (" << total_tests_failed_due_to_error() << " tests) failed due to error\n";
+
 	LOG << total_tests_executed() << " tests executed in total\n";
+	LOG << "Grade: " << letter_grade() << std::endl;
 }
